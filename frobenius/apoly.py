81b966ffd8edd18e11b829b77f663f8385070ccb
Maksim Zholudev <maximzms@gmail.com>
2022-07-01 15:50:27 +0300

    Basic apoly

--- frobenius/apoly.py
+++ frobenius/apoly.py
@@ -1,4 +1,24 @@
+import numpy as np
+
+
 class ArrayPoly:
 
     def __init__(self, coefs):
         self.coefs = coefs
+
+    @property
+    def ndim(self):
+        return self.coefs.ndim - 1
+
+    @property
+    def npow(self):
+        return self.coefs.shape[0]
+
+    @property
+    def shape(self):
+        return self.coefs.shape[1:]
+
+    def __call__(self, x):
+        pows = np.arange(self.npow)
+        xPows = (x ** pows)[(slice(None),) + (np.newaxis,) * self.ndim]
+        return np.sum(self.coefs * xPows, axis=0)
--- test/test_apoly.py
+++ test/test_apoly.py
@@ -4,11 +4,33 @@ import numpy as np
 
 from frobenius.apoly import ArrayPoly
 
+coefs_3 = np.arange(1, 4) * 10
+
+coefs_4_2x3 = \
+    10 * np.arange(1, 5)[:, np.newaxis, np.newaxis] + \
+    np.arange(1, 7).reshape(2, 3)
+
+
 class Test(unittest.TestCase):
     
-    def test_coefs(self):
-        coefs = \
-            np.array([10, 20, 30])[:, np.newaxis, np.newaxis] + \
-            np.array([[1, 2], [3, 4]])
+    def test_basic_3(self):
+        coefs = coefs_3
         a = ArrayPoly(coefs)
         np.testing.assert_allclose(a.coefs, coefs)
+        self.assertEqual(a.ndim, 0)
+        self.assertEqual(a.npow, 3)
+        self.assertEqual(a.shape, ())
+        x = 2
+        np.testing.assert_allclose(a(x),
+            coefs[0] + coefs[1] * x + coefs[2] * x**2)
+
+    def test_basic_4_2x3(self):
+        coefs = coefs_4_2x3
+        a = ArrayPoly(coefs)
+        np.testing.assert_allclose(a.coefs, coefs)
+        self.assertEqual(a.ndim, 2)
+        self.assertEqual(a.npow, 4)
+        self.assertEqual(a.shape, (2, 3))
+        x = 2
+        np.testing.assert_allclose(a(x),
+            coefs[0] + coefs[1] * x + coefs[2] * x**2 + coefs[3] * x**3)
