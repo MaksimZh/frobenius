b703d55a98ab2826679aceb7851c6b802ce60084
Maksim Zholudev <maximzms@gmail.com>
2022-07-05 09:09:17 +0300

    Add test for 3d case

--- test/test_apoly.py
+++ test/test_apoly.py
@@ -4,17 +4,11 @@ import numpy as np
 
 from frobenius.apoly import ArrayPoly
 
-coefs_3 = np.arange(1, 4) * 10
-
-coefs_4_2x3 = \
-    10 * np.arange(1, 5)[:, np.newaxis, np.newaxis] + \
-    np.arange(1, 7).reshape(2, 3)
-
 
 class Test(unittest.TestCase):
     
     def test_basic_3(self):
-        coefs = coefs_3
+        coefs = np.arange(1, 4) * 10
         a = ArrayPoly(coefs)
         np.testing.assert_allclose(a.coefs, coefs)
         self.assertEqual(a.ndim, 0)
@@ -31,7 +25,9 @@ class Test(unittest.TestCase):
             coefs[0] + coefs[1] * x + coefs[2] * x**2)
 
     def test_basic_4_2x3(self):
-        coefs = coefs_4_2x3
+        coefs = \
+            10 * np.arange(1, 5)[:, np.newaxis, np.newaxis] + \
+            np.arange(1, 7).reshape(2, 3)
         a = ArrayPoly(coefs)
         np.testing.assert_allclose(a.coefs, coefs)
         self.assertEqual(a.ndim, 2)
@@ -54,3 +50,28 @@ class Test(unittest.TestCase):
             coefs[1, np.newaxis, np.newaxis] * xa + \
             coefs[2, np.newaxis, np.newaxis] * xa**2 + \
             coefs[3, np.newaxis, np.newaxis] * xa**3)
+
+    def test_basic_3_5x2x4(self):
+        coefs = \
+            100 * np.arange(1, 4)[:, np.newaxis, np.newaxis, np.newaxis] + \
+            np.arange(1, 41).reshape(5, 2, 4)
+        a = ArrayPoly(coefs)
+        np.testing.assert_allclose(a.coefs, coefs)
+        self.assertEqual(a.ndim, 3)
+        self.assertEqual(a.npow, 3)
+        self.assertEqual(a.shape, (5, 2, 4))
+        x = 2
+        np.testing.assert_allclose(a(x),
+            coefs[0] + coefs[1] * x + coefs[2] * x**2)
+        x = np.array([2, 3])
+        xa = x[..., np.newaxis, np.newaxis, np.newaxis]
+        np.testing.assert_allclose(a(x),
+            coefs[0, np.newaxis] + \
+            coefs[1, np.newaxis] * xa + \
+            coefs[2, np.newaxis] * xa**2)
+        x = np.array([[2, 3, 4], [5, 6, 7]])
+        xa = x[..., np.newaxis, np.newaxis, np.newaxis]
+        np.testing.assert_allclose(a(x),
+            coefs[0, np.newaxis, np.newaxis] + \
+            coefs[1, np.newaxis, np.newaxis] * xa + \
+            coefs[2, np.newaxis, np.newaxis] * xa**2)
