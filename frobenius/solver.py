ee1d9de8d12177ed21ec03682b78fe6b9eb65c28
Maksim Zholudev <maximzms@gmail.com>
2022-07-14 15:59:04 +0300

    Start work on solver

--- frobenius/solver.py
+++ frobenius/solver.py
@@ -1,2 +1,46 @@
-def solve():
-    pass
+import numpy as np
+
+from frobenius.apoly import ArrayPoly, det
+from frobenius.smith import smith
+
+
+def solve(mxA, min_terms=3):
+    assert(mxA.ndim == 4)
+    mxL = []
+    for m in range(mxA.shape[2]):
+        mxL.append(ArrayPoly(mxA[:, m]))
+    detL0 = det(mxL[0])
+    lam = np.sort(np.roots(detL0.coefs.reshape(-1)[::-1]))
+    coefsNum = (lam[-1] + min_terms - lam + 0.5).astype(int)
+    for j in range(len(lam)):
+        _calcCoefs(mxL, lam[j], coefsNum[j])
+    return lam
+
+
+def _calcCoefs(mxL, lj, coefsNum):
+    mxSize = mxL[0].shape[0]
+    lamMinusLj = ArrayPoly([-lj, 1])
+    mxX = []
+    mxY = []
+    kappa = []
+    beta = []
+    for n in range(coefsNum):
+        lamPlusN = ArrayPoly([n, 1])
+        x, y, k = smith(mxL[0](lamPlusN), lamMinusLj)
+        mxX.append(x)
+        mxY.append(y)
+        kappa.append(k)
+        beta.append(k[-1])
+    alpha = sum(beta[1:])
+    kernelSize = sum(k > 0 for k in kappa[0])
+    jordanChainsLen = kappa[0][-1 - kernelSize :]
+    ct = np.zeros((kernelSize, coefsNum, max(jordanChainsLen)), dtype=complex)
+    for k in range(kernelSize):
+        for d in range(jordanChainsLen[k]):
+            ct[k, 0, d] = _ort(mxSize, -1 - kernelSize + k + 1)
+
+
+def _ort(n, i):
+    ort = np.zeros(n)
+    ort[i] = 1
+    return ort
--- test/test.py
+++ test/test.py
@@ -8,4 +8,19 @@ from frobenius import solve
 class Test(unittest.TestCase):
 
     def test(self):
-        pass
+        # f' = a f
+        # or
+        # d f - a x f = 0
+        a = 2
+        mxA = np.array([
+            [ # d^0
+                [[0]], # x^0
+                [[-a]], # x^1
+            ],
+            [ # d^1
+                [[1]], # x^0
+                [[0]], # x^1
+            ],
+        ])
+        s = solve(mxA, min_terms=3)
+        self.assertEqual(len(s), 1)
