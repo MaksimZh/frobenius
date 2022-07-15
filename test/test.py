00f050f7a90cb736d72754d9bee1c64da4e74817
Maksim Zholudev <maximzms@gmail.com>
2022-07-15 13:50:16 +0300

    First full test for solver

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ class Test(unittest.TestCase):
         # f' = a f
         # or
         # d f - a x f = 0
-        a = 2
+        a = 3
         mxA = np.array([
             [ # d^0
                 [[0]], # x^0
@@ -24,3 +24,10 @@ class Test(unittest.TestCase):
         ])
         s = solve(mxA, min_terms=4)
         self.assertEqual(len(s), 1)
+        self.assertEqual(len(s[0]), 2)
+        self.assertAlmostEqual(s[0][0], 0)
+        g = s[0][1]
+        self.assertEqual(len(g), 1)
+        self.assertEqual(len(g[0]), 1)
+        gg = g[0][0]
+        np.testing.assert_allclose(gg, [[1], [a], [a**2 / 2], [a**3 / 6]])
