db26a7133506173c2c351ae03392ca86adc86082
Maksim Zholudev <maximzms@gmail.com>
2022-12-16 01:34:28 +0300

    Extract calculation of g

--- frobenius/solver.py
+++ frobenius/solver.py
@@ -37,50 +37,57 @@ def _prepareLambda(mxL0, lambda_roots=None):
 def _calcCoefs(mxL, lj, coefsNum, atol):
     mxSize = mxL[0].shape[0]
     factor = ArrayPoly([-lj, 1])
-    dtype = np.common_type(mxL[0].coefs, factor.coefs)
     mxX, mxY, kappa = \
         _calcSmithN(mxL[0], num=coefsNum, factor=factor, atol=atol)
-    alpha, beta = _calcAlphaBeta(kappa)
-    mxXt = _calcXt(p=factor, x=mxX, kappa=kappa)
-    mxLt = _calcLt(mxL=mxL, p=factor, beta=beta)
     kernelSize = sum(k > 0 for k in kappa[0])
     jordanChainsLen = kappa[0][-kernelSize:]
-    del mxX, factor, kappa
     # TODO - move calculation of ct to separate function
     ct = []
     for k in range(kernelSize):
-        ctk = np.zeros((coefsNum, alpha + jordanChainsLen[k], mxSize, 1),
-            dtype=dtype)
+        ctk = []
         for n in range(coefsNum):
-            nDeriv = beta[n] + jordanChainsLen[k]
             if n == 0:
-                b = [_ort(mxSize, -1 - kernelSize + k + 1)]
+                beta = [0]
             else:
-                b = _calcB(mxY[n], mxLt[n], lj, ctk[:n, :nDeriv])
-            ctk[n, :nDeriv] = _calcCt(mxXt[n], lj, b, nDeriv)
-            del nDeriv, b
+                beta.append(beta[-1] + kappa[n][-1])
+            nDeriv = jordanChainsLen[k] + beta[-1]
+            if n == 0:
+                beta = [0]
+                b = [_ort(mxSize, -1 - kernelSize + k + 1)]
+                mxXt1 = mxX[0]
+            else:
+                mxZ = [trim(factor**(beta[n - 1] - beta[m]) * mxL[n - m](ArrayPoly([m, 1]))) \
+                        for m in range(n)]
+                b = _calcB(mxY[n], mxZ, lj, ctk, nDeriv)
+                mxXt1 = _calcXt1(factor, mxX[n], kappa[n])
+            ctk.append(_calcCt(mxXt1, lj, b, nDeriv))
+            del b
+            del nDeriv
         ct.append(ctk)
         del ctk
-    del mxY, mxXt, mxLt
-    # TODO - move calculation of g to separate function
+    return [_calcG(ctk, atol) for ctk in ct]
+
+
+def _calcG(ct, atol):
+    mxSize = ct[0].shape[1]
+    coefsNum = len(ct)
+    alpha = len(ct[-1]) - len(ct[0])
     g = []
-    for k in range(kernelSize):
-        gk = []
-        for q in range(jordanChainsLen[k]):
-            gkq = np.zeros((coefsNum, alpha + q + 1, mxSize, 1), dtype=complex)
-            for n in range(coefsNum):
-                num_ln_terms = beta[n] + q
-                pow_deriv_coef = factorial(alpha + q) / factorial(num_ln_terms)
-                for m in range(num_ln_terms + 1):
-                    gkq[n, m] = pow_deriv_coef * ct[k][n, num_ln_terms - m]
-                    pow_deriv_coef *= (num_ln_terms - m) / (m + 1)
-                del num_ln_terms, pow_deriv_coef
-            while gkq.shape[1] > 1 and np.max(np.abs(gkq[:, -1])) < atol:
-                gkq = gkq[:, :-1]
-            gk.append(gkq)
-            del gkq
-        g.append(gk)
-        del gk
+    jordanChainsLen = len(ct[0])
+    for q in range(jordanChainsLen):
+        gq = np.zeros((coefsNum, alpha + q + 1, mxSize, 1), dtype=complex)
+        for n in range(coefsNum):
+            beta = len(ct[n]) - jordanChainsLen
+            num_ln_terms = beta + q
+            pow_deriv_coef = factorial(alpha + q) / factorial(num_ln_terms)
+            for m in range(num_ln_terms + 1):
+                gq[n, m] = pow_deriv_coef * ct[n][num_ln_terms - m]
+                pow_deriv_coef *= (num_ln_terms - m) / (m + 1)
+            del beta, num_ln_terms, pow_deriv_coef
+        while gq.shape[1] > 1 and np.max(np.abs(gq[:, -1])) < atol:
+            gq = gq[:, :-1]
+        g.append(gq)
+        del gq
     return g
 
 
@@ -105,24 +112,12 @@ def _calcAlphaBeta(kappa):
     return alpha, beta
 
 
-def _calcXt(p, x, kappa):
-    mxXt = [x[0]]
-    for n in range(1, len(x)):
-        mxXt.append(ArrayPoly(x[n].coefs.copy()))
-        for i in range(len(kappa[n])):
-            mxXt[n][:, i] *= p ** (kappa[n][-1] - kappa[n][i])
+def _calcXt1(p, x, kappa):
+    mxXt = ArrayPoly(x.coefs.copy())
+    for i in range(len(kappa)):
+        mxXt[:, i] *= p ** (kappa[-1] - kappa[i])
     return mxXt
 
-
-def _calcLt(mxL, p, beta):
-    mxLt = [[]]
-    for n in range(1, len(beta)):
-        mxLt.append([])
-        for m in range(n):
-            factor = p ** (beta[n - 1] - beta[m])
-            lamPlusM = ArrayPoly([m, 1])
-            mxLt[n].append(trim(factor * mxL[n - m](lamPlusM)))
-    return mxLt
 
 
 def _ort(n, i):
@@ -138,13 +133,13 @@ def _calcCt(x, lj, b, nTerms):
     return np.array(result)
 
 
-def _calcB(mxY, mxLt, lj, ct):
+def _calcB(mxY, mxLt, lj, ct, nDeriv):
     b = []
     inv_left_matrix = np.linalg.inv(mxY(lj))
-    for t in range(ct.shape[1]):
+    for t in range(nDeriv):
         right_part = sum(
             _derivMatMul(mxLt[m], lj, ct[m], deriv=t) \
-            for m in range(ct.shape[0]))
+            for m in range(len(ct)))
         if len(b) > 0:
             # calculate the t-th derivative of mxY @ b
             # except the term with the t-th derivative of b
