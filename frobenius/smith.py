a2f059bd0b056e447572d91da000a0f102ee5cf6
Maksim Zholudev <maximzms@gmail.com>
2023-04-29 22:48:19 +0300

    Add special matrices for Smith

--- frobenius/smith.py
+++ frobenius/smith.py
@@ -1,6 +1,8 @@
 import numpy as np
+from typing import Any
 
 from frobenius.apoly import ArrayPoly, trim
+from frobenius.tools import Status, status
 
 
 def smith(a, factor, atol=1e-12):
@@ -42,7 +44,6 @@ def smith(a, factor, atol=1e-12):
         left_matrix[:, column] //= factor_powers[diag_factor_exponents[column]]
     return trim(inv_right_matrix, atol), trim(left_matrix, atol), diag_factor_exponents
 
-
 def expandLast(matrix_poly, atol=1e-12):
     coef_columns = matrix_poly.coefs.reshape(-1, matrix_poly.shape[-1])
     u, singular_values, inv_right_matrix = np.linalg.svd(coef_columns)
@@ -55,3 +56,149 @@ def expandLast(matrix_poly, atol=1e-12):
         matrix_poly = np.conj(inv_right_matrix[-1])
     coef_columns = -matrix_poly[:-1] / matrix_poly[-1]
     return coef_columns
+
+# Collects columns added to the right side
+class ColumnCollector(Status):
+
+    _nrows: int
+    _data: list[ArrayPoly]
+    
+    # CONSTRUCTOR
+    # Create empty collector
+    def __init__(self, nrows: int) -> None:
+        super().__init__()
+        self._nrows = nrows
+        self._data = []
+
+
+    # COMMANDS
+
+    # Add column to the right
+    # PRE: `column` is Nx1 polynomial matrix
+    # PRE: `column` height matches `nrows`
+    # POST: `column` is added to the right end of the queue
+    @status("OK", "NOT_COLUMN", "SIZE_MISMATCH")
+    def push_right(self, column: ArrayPoly) -> None:
+        if column.ndim != 2 or column.shape[1] != 1:
+            self._set_status("push_right", "NOT_COLUMN")
+            return
+        if column.shape[0] != self._nrows:
+            self._set_status("push_right", "SIZE_MISMATCH")
+            return
+        self._data.append(column)
+        self._set_status("push_right", "OK")
+
+
+    # QUERIES
+
+    # Check if queue is empty
+    def is_empty(self) -> bool:
+        return len(self._data) == 0
+
+
+class ColumnQueue(ColumnCollector):
+
+    # CONSTRUCTOR
+    # Create empty queue
+    def __init__(self, nrows: int) -> None:
+        super().__init__(nrows)
+
+    
+    # COMMANDS
+
+    # Remove the left column 
+    # PRE: queue is not empty
+    # POST: left column removed
+    @status("OK", "EMPTY")
+    def pop_left(self) -> None:
+        if self.is_empty():
+            self._set_status("pop_left", "EMPTY")
+            return
+        self._data.pop(0)
+        self._set_status("pop_left", "OK")
+
+    
+    # QUERIES
+
+    # Get left column
+    # PRE: queue is not empty
+    @status("OK", "EMPTY")
+    def get_left(self) -> ArrayPoly:
+        if self.is_empty():
+            self._set_status("get_left", "EMPTY")
+            return ArrayPoly(0)
+        self._set_status("get_left", "OK")
+        assert len(self._data) > 0
+        return self._data[0]
+
+
+class ExtendibleMatrix(ColumnCollector):
+
+    # CONSTRUCTOR
+    # Create empty matrix
+    def __init__(self, nrows: int) -> None:
+        super().__init__(nrows)
+
+
+    # QUERIES
+    
+    # Get columns as matrix
+    # PRE: collector is not empty
+    @status("OK", "EMPTY")
+    def get_matrix(self) -> ArrayPoly:
+        if self.is_empty():
+            self._set_status("get_matrix", "EMPTY")
+            return ArrayPoly(0)
+        self._set_status("get_matrix", "OK")
+        max_npow = max(d.npow for d in self._data)
+        ncols = len(self._data)
+        common_type = np.common_type(*[d.coefs for d in self._data])
+        result = ArrayPoly(np.zeros((max_npow, self._nrows, ncols), dtype=common_type))
+        for i in range(ncols):
+            result[:, i : i + 1] = self._data[i]
+        return result
+
+    # Expand given column over matrix columns
+    # PRE: matrix is not empty
+    # PRE: `column` size fits matrix
+    # PRE: `column` is complanar with matrix columns
+    @status("OK", "INVALID_COLUMN", "EMPTY", "NOT_COMPLANAR")
+    def expand_column(self, column: ArrayPoly, atol: float = 1e-12) -> list[complex]:
+        if column.ndim != 2 or column.shape[1] != 1 or column.shape[0] != self._nrows:
+            self._set_status("expand_column", "INVALID_COLUMN")
+            return []
+        if self.is_empty():
+            self._set_status("expand_column", "EMPTY")
+            return []
+        max_npow = max(d.npow for d in self._data)
+        max_npow = max(max_npow, column.npow)
+        ncols = len(self._data)
+        coefs = np.zeros((max_npow * self._nrows, ncols + 1))
+        for i in range(ncols):
+            c = self._data[i].coefs.reshape(-1)
+            coefs[:len(c), i] = c
+        c = column.coefs.reshape(-1)
+        coefs[:len(c), -1] = c
+        u, s, vh = np.linalg.svd(coefs)
+        if s[-1] > atol:
+            self._set_status("expand_column", "NOT_COMPLANAR")
+            return []
+        self._set_status("expand_column", "OK")
+        fitting_coefs = np.conj(vh[-1])
+        expansion_coefs = -fitting_coefs[:-1] / fitting_coefs[-1]
+        return list(expansion_coefs)
+
+    # Make linear combination of columns with given coefficients
+    # PRE: matrix is not empty
+    # PRE: `factors` size fits number of columns
+    @status("OK", "EMPTY", "SIZE_MISMATCH")
+    def combine_columns(self, factors: list[Any]) -> ArrayPoly:
+        if self.is_empty():
+            self._set_status("combine_columns", "EMPTY")
+            return ArrayPoly(0)
+        if len(factors) != len(self._data):
+            self._set_status("combine_columns", "SIZE_MISMATCH")
+            return ArrayPoly(0)
+        self._set_status("combine_columns", "OK")
+        return sum((f * c for f, c in zip(factors, self._data)),
+                   start=ArrayPoly([[[0]] * self._nrows]))
--- test/test_smith.py
+++ test/test_smith.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 
 
-from frobenius.apoly import ArrayPoly
+from frobenius.apoly import ArrayPoly, trim
 from frobenius.smith import smith
 
 
@@ -85,3 +85,182 @@ class Test(unittest.TestCase):
         p = ArrayPoly([-3, 1])
         x, y, kappa = smith(a, p)
         self.check(a, p, x, y, kappa)
+
+
+from frobenius.smith import ColumnCollector, ColumnQueue, ExtendibleMatrix
+
+class Test_ColumnCollector(unittest.TestCase):
+
+    def test_init(self):
+        a = ColumnCollector(2)
+        self.assertTrue(a.is_empty())
+
+    def test_push(self):
+        a = ColumnCollector(2)
+        self.assertTrue(a.is_status("push_right", "NIL"))
+        a.push_right(ArrayPoly([[1, 2]]))
+        self.assertTrue(a.is_status("push_right", "NOT_COLUMN"))
+        a.push_right(ArrayPoly([[[1], [2], [3]]]))
+        self.assertTrue(a.is_status("push_right", "SIZE_MISMATCH"))
+        a.push_right(ArrayPoly([[[1], [2]]]))
+        self.assertTrue(a.is_status("push_right", "OK"))
+        self.assertFalse(a.is_empty())
+
+class Test_ColumnQueue(unittest.TestCase):
+    
+    def test_pop(self):
+        a = ColumnQueue(2)
+        self.assertTrue(a.is_status("pop_left", "NIL"))
+        a.pop_left()
+        self.assertTrue(a.is_status("pop_left", "EMPTY"))
+        a.push_right(ArrayPoly([[[1], [2]]]))
+        a.pop_left()
+        self.assertTrue(a.is_status("pop_left", "OK"))
+        self.assertTrue(a.is_empty())
+        a.push_right(ArrayPoly([[[1], [2]]]))
+        a.push_right(ArrayPoly([[[1], [2]]]))
+        a.push_right(ArrayPoly([[[1], [2]]]))
+        a.pop_left()
+        self.assertTrue(a.is_status("pop_left", "OK"))
+        a.pop_left()
+        self.assertTrue(a.is_status("pop_left", "OK"))
+        a.pop_left()
+        self.assertTrue(a.is_status("pop_left", "OK"))
+        a.pop_left()
+        self.assertTrue(a.is_status("pop_left", "EMPTY"))
+    
+    def test_get(self):
+        p1 = ArrayPoly([[[1], [2]], [[3], [4]]])
+        p2 = ArrayPoly([[[5], [6]]])
+        p3 = ArrayPoly([[[7], [8]], [[9], [10]], [[11], [12]]])
+        a = ColumnQueue(2)
+        self.assertTrue(a.is_status("get_left", "NIL"))
+        c = a.get_left()
+        self.assertTrue(a.is_status("get_left", "EMPTY"))
+        a.push_right(p1)
+        a.push_right(p2)
+        a.push_right(p3)
+        c = a.get_left()
+        self.assertTrue(a.is_status("get_left", "OK"))
+        np.testing.assert_almost_equal(c.coefs, p1.coefs)
+        a.pop_left()
+        c = a.get_left()
+        self.assertTrue(a.is_status("get_left", "OK"))
+        np.testing.assert_almost_equal(c.coefs, p2.coefs)
+        a.pop_left()
+        c = a.get_left()
+        self.assertTrue(a.is_status("get_left", "OK"))
+        np.testing.assert_almost_equal(c.coefs, p3.coefs)
+        a.pop_left()
+        c = a.get_left()
+        self.assertTrue(a.is_status("get_left", "EMPTY"))
+
+    def test_rotate_and_clear(self):
+        p = [
+            ArrayPoly([[[1], [2]], [[3], [4]]]),
+            ArrayPoly([[[5], [6]]]),
+            ArrayPoly([[[7], [8]], [[9], [10]], [[11], [12]]]),
+        ]
+        a = ColumnQueue(2)
+        a.push_right(p[0])
+        a.push_right(p[1])
+        a.push_right(p[2])
+        np.testing.assert_array_almost_equal(a.get_left().coefs, p[0].coefs)
+        a.pop_left()
+        a.push_right(p[0] * 2)
+        np.testing.assert_array_almost_equal(a.get_left().coefs, p[1].coefs)
+        a.pop_left()
+        a.push_right(p[1] * 2)
+        np.testing.assert_array_almost_equal(a.get_left().coefs, p[2].coefs)
+        a.pop_left()
+        a.push_right(p[2] * 2)
+        np.testing.assert_array_almost_equal(a.get_left().coefs, p[0].coefs * 2)
+        a.pop_left()
+        np.testing.assert_array_almost_equal(a.get_left().coefs, p[1].coefs * 2)
+        a.pop_left()
+        np.testing.assert_array_almost_equal(a.get_left().coefs, p[2].coefs * 2)
+        a.pop_left()
+        self.assertTrue(a.is_empty())
+
+
+class Test_ExtendibleMatrix(unittest.TestCase):
+
+    def test_get_matrix(self):
+        p = [
+            ArrayPoly([[[1], [2]], [[3], [4]]]),
+            ArrayPoly([[[5], [6]]]),
+            ArrayPoly([[[7], [8]], [[9], [10]], [[11], [12]]]),
+        ]
+        a = ExtendibleMatrix(2)
+        self.assertTrue(a.is_status("get_matrix", "NIL"))
+        m = a.get_matrix()
+        self.assertTrue(a.is_status("get_matrix", "EMPTY"))
+        a.push_right(p[0])
+        a.push_right(p[1])
+        a.push_right(p[2])
+        m = a.get_matrix()
+        self.assertTrue(a.is_status("get_matrix", "OK"))
+        np.testing.assert_almost_equal(
+            m.coefs,
+            [
+                [
+                    [1, 5, 7],
+                    [2, 6, 8],
+                ],
+                [
+                    [3, 0, 9],
+                    [4, 0, 10],
+                ],
+                [
+                    [0, 0, 11],
+                    [0, 0, 12],
+                ],
+            ])
+
+    def test_expand_column(self):
+        p = [
+            ArrayPoly([[[1], [2]], [[3], [4]]]),
+            ArrayPoly([[[5], [6]]]),
+            ArrayPoly([[[7], [8]], [[9], [10]], [[11], [12]]]),
+        ]
+        a = ExtendibleMatrix(2)
+        self.assertTrue(a.is_status("expand_column", "NIL"))
+        c = a.expand_column(p[0])
+        self.assertTrue(a.is_status("expand_column", "EMPTY"))
+        a.push_right(p[0])
+        a.push_right(p[1])
+        c = a.expand_column(ArrayPoly([[1]]))
+        self.assertTrue(a.is_status("expand_column", "INVALID_COLUMN"))
+        c = a.expand_column(p[0])
+        self.assertTrue(a.is_status("expand_column", "OK"))
+        np.testing.assert_almost_equal(c, [1, 0])
+        c = a.expand_column(p[1])
+        self.assertTrue(a.is_status("expand_column", "OK"))
+        np.testing.assert_almost_equal(c, [0, 1])
+        c = a.expand_column(p[2])
+        self.assertTrue(a.is_status("expand_column", "NOT_COMPLANAR"))
+        c = a.expand_column(2 * p[0] + 3 * p[1])
+        self.assertTrue(a.is_status("expand_column", "OK"))
+        np.testing.assert_almost_equal(c, [2, 3])
+
+    def test_combine_columns(self):
+        p = [
+            ArrayPoly([[[1], [2]], [[3], [4]]]),
+            ArrayPoly([[[5], [6]]]),
+            ArrayPoly([[[7], [8]], [[9], [10]], [[11], [12]]]),
+        ]
+        a = ExtendibleMatrix(2)
+        self.assertTrue(a.is_status("combine_columns", "NIL"))
+        c = a.combine_columns([1])
+        self.assertTrue(a.is_status("combine_columns", "EMPTY"))
+        a.push_right(p[0])
+        a.push_right(p[1])
+        a.push_right(p[2])
+        c = a.combine_columns([1])
+        self.assertTrue(a.is_status("combine_columns", "SIZE_MISMATCH"))
+        f = [2, ArrayPoly([3, 4]), 5]
+        c = a.combine_columns(f)
+        self.assertTrue(a.is_status("combine_columns", "OK"))
+        np.testing.assert_almost_equal(
+            c.coefs,
+            (p[0] * f[0] + p[1] * f[1] + p[2] * f[2]).coefs)
